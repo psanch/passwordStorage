318c5de859b70b6e78067824369d42ff9c6f316e
Pedro Sanchez <pedrosanchezm97@gmail.com>
2018-11-24 17:29:46 -0800

    added createAccount, will not write username if username already exists.

--- createAccount.py
+++ createAccount.py
@@ -1,8 +1,10 @@
 #Usage: python3 createAccount.py <username> <salt> <H(s+pw)>
 
 import sys
+import os
 
 filename_logins = 'logins.txt'
+filename_row = 'row.txt'
 
 def createAccountRecord(args):
 	username = args[0]
@@ -15,10 +17,27 @@ def createAccountRecord(args):
 	fp.write(contents)
 	fp.close()
 
+def usernameIsUnique(username):
+	msg = "python3 getRowFromUsername.py " + str(username)
+	os.system(msg)
+
+	fp = open(filename_row,'r')
+	results = [(login.strip('\n')).split() for login in fp.readlines()]
+	fp.close()
+
+	if results[0][0] != "-1":
+		return False
+	return True
+
 
 def main(argv):
 	if(len(argv) != 4):
 		print('Incorrect argument count')
+		return
+
+	if(usernameIsUnique(argv[1]) == False):
+		print('username already in use')
+		return
 
 	createAccountRecord(argv[1:])
 
